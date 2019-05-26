8985cf7bcd0029cd7136f5f4ff8bd11f9adf1a35
vladislao <netcemetery@yandex.ru>
2019-05-26 17:51:16 +0400

    Update example
    Closes #8

--- example/index.js
+++ example/index.js
@@ -1,22 +1,42 @@
 const http = require("http");
+const { resolve } = require("path");
 const fs = require("fs");
-const balancer = require("../lib/balancer");
-const monitor = require("../lib/monitor");
+const supervisor = require("../index");
 
 const proxies = fs
-  .readFileSync("./proxy.txt", "utf-8")
+  .readFileSync(resolve(__dirname, "proxy.txt"), "utf-8")
   .split("\n")
   .map(x => x.trim())
   .filter(x => x);
-const fileBalancer = balancer()
-  .source(monitor)
-  .add(proxies);
 
+if (proxies[0] === "Put your proxy list here") {
+  console.error("Please modify your proxy.txt file first");
+  process.exit(1);
+}
+
+const balancer = supervisor.balancer().add(proxies);
+
+if (balancer.proxies.size === 0) {
+  if (proxies.length > 0) {
+    console.error("Check proxies format, none of them are valid");
+  } else {
+    console.error(
+      "Your proxy.txt file is empty, please add some proxies first"
+    );
+  }
+  process.exit(1);
+}
+
+balancer.subscribe(supervisor.monitor());
+
+const middleware = balancer.proxy();
 const server = http.createServer((req, res) => {
   console.log(req.url);
-  return fileBalancer.proxy()(req, res, err => {
+  return middleware(req, res, err => {
     console.log(err);
   });
 });
 
-server.listen(9999);
+server.listen(9999, () => {
+  console.log("Listening on port 9999");
+});
